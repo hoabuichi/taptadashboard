c0a98fc754653654b7a9302c85b7bf47a22b4c95
Bui Chi Hoa <buichihoa92@gmail.com>
2023-08-10 22:47:53 +0700

    payment tab UI

--- app/src/main/java/com/example/taptadashboard/payment/Keypad.kt
+++ app/src/main/java/com/example/taptadashboard/payment/Keypad.kt
@@ -1,16 +1,16 @@
 package com.example.taptadashboard.payment
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager2.widget.ViewPager2
-import com.example.taptadashboard.R
-import com.example.taptadashboard.databinding.ActivityMainBinding
 import com.example.taptadashboard.databinding.FragmentKeypadBinding
 import com.google.android.material.tabs.TabLayout
+
 
 /**
  * A simple [Fragment] subclass.
