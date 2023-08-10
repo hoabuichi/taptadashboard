155838bb8ad496eb7b5a871ead6922c043736a26
Bui Chi Hoa <buichihoa92@gmail.com>
2023-08-10 23:25:51 +0700

    quick payment

--- app/src/main/java/com/example/taptadashboard/payment/QuickPayment.kt
+++ app/src/main/java/com/example/taptadashboard/payment/QuickPayment.kt
@@ -7,11 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.taptadashboard.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [QuickPayment.newInstance] factory method to
@@ -19,15 +14,9 @@ private const val ARG_PARAM2 = "param2"
  */
 class QuickPayment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -51,10 +40,7 @@ class QuickPayment : Fragment() {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             QuickPayment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
             }
     }
 }
